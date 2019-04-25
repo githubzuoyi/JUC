4327b298018a30a5dd2a9c22eaeb8881d46a4dd4
zuofei <1783164557@qq.com>
2019-04-25 23:06:57 +0800

    add commit "dataStruggle"

---
+++ src/com/feizuo/java4/Test2.java
@@ -0,0 +1,14 @@
+package com.feizuo.java4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test2 class
+ *
+ * @author 左飞
+ * @date 2019/4/25
+ */
+public class Test2 {
+    //List<Integer> l = new ArrayList<Integer>(5)
+}
